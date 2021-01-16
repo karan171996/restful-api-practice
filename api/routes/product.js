eee52549dfa4b350e10bcfc0f42da086ec2010bf
Karan Singh <krnsngh38@gmail.com>
2021-01-17 02:08:10 +0530

    patch delete and monogo connect

--- api/routes/product.js
+++ api/routes/product.js
@@ -1,46 +1,88 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello",
-  });
+  Product.find()
+    .exec()
+    .then((product) => res.status(200).json(product))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-  };
-  res.status(201).json({
-    message: "Hello",
-    createdProduct: product,
   });
+  product
+    .save()
+    .then((product) => {
+      console.log("Product saved");
+      res.status(201).json({
+        message: "Hello",
+        createdProduct: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res.status(200).json({
-      message: "Special",
-      id: id,
+  Product.findById(id)
+    .then((product) => {
+      if (product) {
+        res.status(200).json(product);
+      } else {
+        res.status(400).json({
+          message: "No valid id",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: "Passed Id",
-    });
-  }
 });
 
 router.patch("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated product",
-  });
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops in req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((product) => {
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.delete("/:productId", (req, res, next) => {
-  res.status(200).json({
-    message: "Delete product",
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then((product) => {
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 module.exports = router;
--- app.js
+++ app.js
@@ -3,14 +3,34 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/product");
 const orderRouter = require("./api/routes/orders");
 
+mongoose.connect(
+  `mongodb+srv://Karan17:${process.env.MONGO_ATLAS_PSWD}@rest-api.nnioy.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,DELETE,GET,PATCH");
+    return res.status(200).json({});
+  }
+  next();
+});
 app.use("/products", productRoutes);
 app.use("/orders", orderRouter);
 
